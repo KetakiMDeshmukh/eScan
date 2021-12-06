a062f54cdfc994c3cbcb0c50052c62eb80280f09
KetakiMDeshmukh <84900720+KetakiMDeshmukh@users.noreply.github.com>
2021-12-06 23:46:00 +0530

    Update menu_page.dart

--- lib/menu_page.dart
+++ lib/menu_page.dart
@@ -13,7 +13,7 @@ class NavDrawer extends StatelessWidget {
               style: TextStyle(color: Colors.white, fontSize: 25),
             ),
             decoration: BoxDecoration(
-                color: Colors.green,
+                color: Colors.blue,
                 image: DecorationImage(
                     fit: BoxFit.fill,
                     image: AssetImage('assets/images/cover.jpg'))),
